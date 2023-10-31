55660bdb7e7dc60ed49832070f599507e2dcbb31
Ashley Pridgeon <ashley.pridgeon@bristol.ac.uk>
2023-10-31 07:08:48 +0000

    adding file

---
+++ src/general/direction.ts
@@ -0,0 +1,23 @@
+import { Position } from "../plant/plantData";
+
+
+
+export enum Direction {
+    North  = 'N',
+    East = 'E',
+    South = 'S',
+    West = 'W',
+    None = 'NA'
+}
+
+export class DirectionVectors {
+
+    static vectors = new Map<Direction, Position>([
+        [Direction.North, {x: 0, y: -1}],
+        [Direction.East, {x: 1, y: 0}],
+        [Direction.South, {x: 0, y: 1}],
+        [Direction.West, {x: -1, y: 0}]
+
+    ])
+
+}
