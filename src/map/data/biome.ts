2602f392be691dc6442252ae28594c55485cf292
Ashley <ashleypridgeon@gmail.com>
2024-03-31 16:20:23 +0100

    adding file

---
+++ src/map/data/biome.ts
@@ -0,0 +1,153 @@
+import Game_Config from "../../game_config";
+import LandData from "./landData";
+import { LandTypes } from "./landGenerator";
+import MapData from "./mapData";
+import { RuleTile } from "../display/ruleTileSets";
+import * as Phaser from "phaser";
+
+const ROWLENGTH = 25;
+
+export enum BiomeType {
+    Grassland,
+    Sandy
+}
+
+export enum BiomeTiles {
+    Vines,
+    Grassland,
+    Mushrooms,
+    SandSurface
+}
+
+export class BiomeTileSets {
+    static testSet = new Map<BiomeTiles, integer[]>([
+        [BiomeTiles.Vines, [(6 * ROWLENGTH) + 9, (6 * ROWLENGTH) + 10]],
+        [BiomeTiles.Grassland, [(6 * ROWLENGTH) + 11, (6 * ROWLENGTH) + 12, (6 * ROWLENGTH) + 13, (6 * ROWLENGTH) + 14]],
+        [BiomeTiles.Mushrooms, [(22 * ROWLENGTH) + 4]],
+        [BiomeTiles.SandSurface, [(7*ROWLENGTH) + 9, (7*ROWLENGTH) + 10]]
+
+    ])
+}
+
+
+
+export default class Biome {
+
+    _mapData: MapData;
+    _scene: Phaser.Scene;
+
+    constructor(mapData: MapData, scene: Phaser.Scene){
+        this._mapData = mapData;
+        this._scene = scene;
+
+        this.setBiome();
+
+
+       
+    }
+
+// TODO: sort out the creation of different biomes
+    setBiome(){
+
+        let biomeChoices = [BiomeType.Grassland, BiomeType.Grassland, BiomeType.Grassland, BiomeType.Sandy]
+        let biomeSizes = 50;
+
+        for(let xChunk = 0; xChunk<Game_Config.MAP_SIZE.x; xChunk+=biomeSizes){
+
+            let biome = biomeChoices[Math.floor(Math.random()*biomeChoices.length)];
+            let landType = (biome == BiomeType.Sandy) ? LandTypes.Sandy : LandTypes.Normal;
+
+            for(let y = 0; y < Game_Config.MAP_SIZE.y; y++){
+
+                let xWobble = this._mapData._mapManager.noise.simplex2(0.5, y * 0.05);
+                let xWobbleRounded = Phaser.Math.RoundTo(xWobble * 5, 0) - 5;
+
+                for(let x = xChunk + xWobbleRounded; x <xChunk + biomeSizes - xWobbleRounded; x++){
+
+                    if(this._mapData.landGenerator.landData[y][x]){
+                        let land = this._mapData.landGenerator.landData[y][x];
+                        if(land.isLand()){
+                            land.biomeType = biome;
+                            land.landType = landType;
+                            land.initStrength();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    addImages(){
+        this._mapData.landGenerator.landData.forEach((row, y) => {
+            row.forEach((land, x) => {
+                this.addVines(land, x, y);
+                this.addGrass(land, x, y);
+                this.addMushrooms(land, x, y);
+                this.addSandSurface(land, x, y);
+            })
+        })
+    }
+
+
+    addVines(land: LandData, x: integer, y: integer){
+        if(y > 0){
+            let aboveTile = this._mapData.landGenerator.landData[y - 1][x];
+            if((land.ruleTile == RuleTile.top || land.ruleTile == RuleTile.topLeft || land.ruleTile == RuleTile.topRight) && land.landType == LandTypes.Normal && aboveTile.landType == LandTypes.Hole){
+                let indeces = BiomeTileSets.testSet.get(BiomeTiles.Vines)
+                let index = indeces[Math.floor(Math.random()*indeces.length)]
+                land.biome = this._scene.add.image(Game_Config.MAP_tilesToWorld(land.pos.x), Game_Config.MAP_tilesToWorld(land.pos.y), 'plantTilesSpriteSheet', index)
+                    .setOrigin(0)
+                    .setDepth(5)
+                    .setScale(Game_Config.MAP_SCALE);
+            }
+        }
+    }
+
+    addGrass(land: LandData, x: integer, y: integer){
+        if(y > 0){
+            let aboveTile = this._mapData.landGenerator.landData[y - 1][x];
+            console.log(land.ruleTile)
+            if((land.ruleTile == RuleTile.top || land.ruleTile == RuleTile.topLeft || land.ruleTile == RuleTile.topRight) && land.landType == LandTypes.Normal && aboveTile.landType == LandTypes.None){
+                console.log("hello!")
+                let indeces = BiomeTileSets.testSet.get(BiomeTiles.Grassland)
+                let index = indeces[Math.floor(Math.random()*indeces.length)]
+                land.biome = this._scene.add.image(Game_Config.MAP_tilesToWorld(land.pos.x), Game_Config.MAP_tilesToWorld(land.pos.y - 1), 'plantTilesSpriteSheet', index)
+                    .setOrigin(0)
+                    .setDepth(5)
+                    .setScale(Game_Config.MAP_SCALE);
+            }
+        }
+    }
+
+    addMushrooms(land: LandData, x: integer, y: integer){
+        if(y > 0){
+            let rand = Math.random();
+            let aboveTile = this._mapData.landGenerator.landData[y - 1][x];
+            if((land.ruleTile == RuleTile.top || land.ruleTile == RuleTile.topLeft || land.ruleTile == RuleTile.topRight) && land.landType == LandTypes.Normal && aboveTile.landType == LandTypes.Hole && rand < 0.2){
+                let indeces = BiomeTileSets.testSet.get(BiomeTiles.Mushrooms)
+                let index = indeces[Math.floor(Math.random()*indeces.length)]
+                land.biome = this._scene.add.image(Game_Config.MAP_tilesToWorld(land.pos.x), Game_Config.MAP_tilesToWorld(land.pos.y - 1), 'plantTilesSpriteSheet', index)
+                    .setOrigin(0)
+                    .setDepth(5)
+                    .setScale(Game_Config.MAP_SCALE);
+            }
+        }
+    }
+
+    addSandSurface(land: LandData, x: integer, y: integer){
+        if(y > 0){
+            let aboveTile = this._mapData.landGenerator.landData[y - 1][x];
+            if((land.ruleTile == RuleTile.top || land.ruleTile == RuleTile.topLeft || land.ruleTile == RuleTile.topRight) && land.landType == LandTypes.Sandy && aboveTile.landType == LandTypes.None){
+                let indeces = BiomeTileSets.testSet.get(BiomeTiles.SandSurface)
+                let index = indeces[Math.floor(Math.random()*indeces.length)]
+                land.biome = this._scene.add.image(Game_Config.MAP_tilesToWorld(land.pos.x), Game_Config.MAP_tilesToWorld(land.pos.y - 1), 'plantTilesSpriteSheet', index)
+                    .setOrigin(0)
+                    .setDepth(5)
+                    .setScale(Game_Config.MAP_SCALE);
+            }
+        }
+    }
+
+
+
+}
