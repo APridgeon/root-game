1049d93b1477f62fe910823c02906998177d0f95
Ashley <ashleypridgeon@gmail.com>
2026-08-27 07:11:18 +0100

    Modified water text objects in UI

--- src/UI/UI_scene.ts
+++ src/UI/UI_scene.ts
@@ -23,17 +23,13 @@ export default class UI extends Phaser.Scene {
   barometer: Barometer;
   fullscreenButton: Button;
   soundButton: Button;
-
-  waterText: Phaser.GameObjects.BitmapText;
-  waterRemovedText: Phaser.GameObjects.BitmapText;
-  waterAddedText: Phaser.GameObjects.BitmapText;
   uiText: Phaser.GameObjects.BitmapText;
+  waterTextObjects: Record<string, Phaser.GameObjects.BitmapText> = {};
 
   turnNo = 0;
 
   constructor() {
     super({ key: 'UI' })
-
   }
 
   preload() {
@@ -47,39 +43,84 @@ export default class UI extends Phaser.Scene {
 
   create() {
 
-    this.uiTileMap = this.make.tilemap({ tileHeight: Game_Config.UI_RES, tileWidth: Game_Config.UI_RES, height: Game_Config.MAP_SIZE.y, width: Game_Config.MAP_SIZE.x });
-    this.uiTiles = this.uiTileMap.addTilesetImage('UI_tiles', 'UI_tiles', Game_Config.UI_RES, Game_Config.UI_RES, 0, 0);
+    this.uiTileMap = this.make.tilemap({
+      tileHeight: Game_Config.UI_RES,
+      tileWidth: Game_Config.UI_RES,
+      height: Game_Config.MAP_SIZE.y,
+      width: Game_Config.MAP_SIZE.x
+    });
 
-    let welcomeBox = new TextBox("WELCOME TO TAPROOT!", this, this.uiTileMap, UI_TileSets.boxStyle3, 0, -Game_Config.UI_tilesToWorld(5), 20, 5)
-    let turnBox = new TextBox('Turn no: 0', this, this.uiTileMap, UI_TileSets.boxStyle3, gameManager.mobile ? 0 : -Game_Config.UI_tilesToWorld(8), gameManager.mobile ? 0 : -Game_Config.UI_tilesToWorld(5), 8, 5);
+    this.uiTiles = this.uiTileMap.addTilesetImage(
+      'UI_tiles',
+      'UI_tiles',
+      Game_Config.UI_RES,
+      Game_Config.UI_RES,
+      0,
+      0
+    );
+
+    const welcomeBox = new TextBox(
+      "WELCOME TO TAPROOT!",
+      this,
+      this.uiTileMap,
+      UI_TileSets.boxStyle3,
+      0,
+      -Game_Config.UI_tilesToWorld(5),
+      20, 5
+    )
+
+    const turnBox = new TextBox(
+      'Turn no: 0',
+      this, this.uiTileMap,
+      UI_TileSets.boxStyle3,
+      gameManager.mobile ? 0 : -Game_Config.UI_tilesToWorld(8),
+      gameManager.mobile ? 0 : -Game_Config.UI_tilesToWorld(5),
+      8, 5
+    );
 
     new GuideManager(this);
 
     this.barometer = new Barometer(this, this.uiTileMap, Game_Config.UI_tilesToWorld(1), this.game.scale.height - Game_Config.UI_tilesToWorld(7), 10);
 
-    this.waterText = this.add.bitmapText(Game_Config.UI_tilesToWorld(2), this.game.scale.height - Game_Config.UI_tilesToWorld(8), 'ant_party', 'Water: ' + Game_Config.PLANT_DATA_WATER_START_LEVEL)
-      .setOrigin(0, 0)
-      .setTint(0x0095e9)
-      .setScale(Game_Config.FONT_SCALE)
-      .setDepth(10);
+    const waterTextContext: { key: string, text: string, colour: number }[] = [
+      {
+        key: 'water level',
+        text: 'Water: ' + Game_Config.PLANT_DATA_WATER_START_LEVEL,
+        colour: 0x0095e9
+      },
+      {
+        key: 'water removed',
+        text: 'Water removed: ' + 0,
+        colour: 0xff0000
+      },
+      {
+        key: 'water added',
+        text: 'Water added: ' + 0,
+        colour: 0x00ff00
+      },
+    ]
 
-    this.waterRemovedText = this.add.bitmapText(Game_Config.UI_tilesToWorld(2), this.game.scale.height - Game_Config.UI_tilesToWorld(9), 'ant_party', 'Water removed: ' + 0)
-      .setOrigin(0, 0)
-      .setTint(0xff0000)
-      .setScale(Game_Config.FONT_SCALE)
-      .setDepth(10);
+    waterTextContext.forEach((context, i) => {
+      const waterTextObject = this.add.bitmapText(
+        Game_Config.UI_tilesToWorld(2),
+        this.game.scale.height - Game_Config.UI_tilesToWorld(10 - i),
+        'ant_party',
+        context.text
+      )
+        .setOrigin(0, 0)
+        .setTint(context.colour)
+        .setScale(Game_Config.FONT_SCALE)
+        .setDepth(10);
 
-    this.waterAddedText = this.add.bitmapText(Game_Config.UI_tilesToWorld(2), this.game.scale.height - Game_Config.UI_tilesToWorld(10), 'ant_party', 'Water added: ' + 0)
-      .setOrigin(0, 0)
-      .setTint(0x00ff00)
-      .setScale(Game_Config.FONT_SCALE)
-      .setDepth(10);
+      this.waterTextObjects[context.key] = waterTextObject
+
+    })
 
 
     this.scene.get('main').events.on(Events.WaterText, (waterStats: WaterStats) => {
-      this.waterText.setText('Water: ' + waterStats.totalWater.toString());
-      this.waterRemovedText.setText('Water removed: ' + waterStats.waterRemoved);
-      this.waterAddedText.setText('Water added: ' + waterStats.waterAdded);
+      this.waterTextObjects['water level'].setText('Water: ' + waterStats.totalWater.toString())
+      this.waterTextObjects['water removed'].setText('Water removed: ' + waterStats.waterRemoved)
+      this.waterTextObjects['water added'].setText('Water added: ' + waterStats.waterAdded)
       this.barometer.setWaterBar(waterStats.totalWater);
     })
 
@@ -121,15 +162,11 @@ export default class UI extends Phaser.Scene {
 
   }
 
-
   private resize(screenDim: Position) {
     this.barometer.setPosition({ x: Game_Config.UI_tilesToWorld(1), y: screenDim.y - Game_Config.UI_tilesToWorld(7) });
-    this.waterText.setPosition(Game_Config.UI_tilesToWorld(2), screenDim.y - Game_Config.UI_tilesToWorld(8));
-    this.waterRemovedText.setPosition(Game_Config.UI_tilesToWorld(2), screenDim.y - Game_Config.UI_tilesToWorld(9));
-    this.waterAddedText.setPosition(Game_Config.UI_tilesToWorld(2), screenDim.y - Game_Config.UI_tilesToWorld(10));
-
+    this.waterTextObjects['water level'].setPosition(Game_Config.UI_tilesToWorld(2), screenDim.y - Game_Config.UI_tilesToWorld(8));
+    this.waterTextObjects['water removed'].setPosition(Game_Config.UI_tilesToWorld(2), screenDim.y - Game_Config.UI_tilesToWorld(9));
+    this.waterTextObjects['water added'].setPosition(Game_Config.UI_tilesToWorld(2), screenDim.y - Game_Config.UI_tilesToWorld(10));
   }
-
-
 
 }
